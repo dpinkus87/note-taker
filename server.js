992804328611e9a9d74aa48b7e516d55eb198d5a
dpinkus87 <dylanpinkus@gmail.com>
2022-12-13 22:50:16 -0600

    Connected api

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 const db = require('./db/db');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.use(express.static('public'));
 
 // COMPLETED: `GET /notes` should return the `notes.html` file.
 
-app.get('/', (req, res) =>
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
@@ -28,32 +29,43 @@ app.get('/', (req, res) =>
 
 // TODO: `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
 
-app.get('/db/db', (req, res) => {
+app.get('/api/notes', (req, res) => {
     res.status(200).json(db);
+
+    console.info(`${req.method} request received to get notes`);
+
   });
 
 // * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
-app.post('/api/reviews', (req, res) => {
+app.post('/api/notes', (req, res) => {
     // Log that a POST request was received
-    console.info(`${req.method} request received to add a review`);
+    console.info(`${req.method} request received to add a note`);
   
-    // Prepare a response object to send back to the client
-    let response;
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
   
-    // Check if there is anything in the response body
-    if (req.body && req.body.product) {
-      response = {
-        status: 'success',
-        data: req.body,
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        note_id: uuidv4(),
       };
-      res.json(`Review for ${response.data.product} has been added!`);
-    } else {
-      res.json('Request body must at least contain a product name');
-    }
+
+      readAndAppend(newNote, './db/notes.json');
+
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
   
-    // Log the response body to the console
-    console.log(req.body);
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting note');
+    }
   });
 
 
