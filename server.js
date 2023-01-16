67101aa9f370a81ec06021b27ea05b0a4dcd65f6
dpinkus87 <dylanpinkus@gmail.com>
2023-01-16 09:38:47 -0600

    Updated uuid reference

--- server.js
+++ server.js
@@ -1,48 +1,44 @@
 const express = require('express');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-
-const db = require('./db/db');
+const util = require('util');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.static('public'));
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // The following HTML routes should be created:
 
-// COMPLETED: `GET /notes` should return the `notes.html` file.
+// COMPLETED: `GET *` should return the `index.html` file.
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// COMPLETED: `GET *` should return the `index.html` file.
+const readFromFile = util.promisify(fs.readFile);
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// COMPLETED: `GET /notes` should return the `notes.html` file.
 
-const readFile = util.promisify(fs.readFile)
+app.get('/api/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+
+
 
 // The following API routes should be created:
 
 // TODO: `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
 
-app.get('/api/notes', (req, res) => {
-    res.status(200).json(db);
-
-    console.info(`${req.method} request received to get notes`);
-
-  });
-
 // * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
 app.post('/api/notes', (req, res) => {
-    // Log that a POST request was received
-    console.info(`${req.method} request received to add a note`);
+  console.info(`${req.method} request received to add notes`);
   
     // Destructuring assignment for the items in req.body
     const { title, text } = req.body;
@@ -56,19 +52,19 @@ app.post('/api/notes', (req, res) => {
         note_id: uuidv4(),
       };
 
-      fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-        if(err) {
+      fs.readFile('./db/db.json', `utf-8`, (err, data) => {
+        if (err) {
           console.log(err);
         } else {
           const notes = JSON.parse(data);
           notes.push(newNote);
-
-          fs.writeFile('./db/db.json', JSON.stringify(notes, null, '\t'), (err) => 
-          err
-          ? console.error(err)
-          : console.log(
-            'Notes for ${newNote.title} has been written to JSON file'
-          )
+  
+          fs.writeFile(`./db/db.json`, JSON.stringify(notes, null, `\t`), (err) =>
+            err
+              ? console.error(err)
+              : console.log(
+                `Notes for ${newNote.title} has been written to JSON file`
+              )
           );
         }
       })
@@ -85,6 +81,9 @@ app.post('/api/notes', (req, res) => {
     }
   });
 
+  app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
   //-------//
 app.listen(PORT, () =>
